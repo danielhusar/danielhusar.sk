a330738940d9a9646a1a329d5c46b164a044050c
Daniel Husar <dano.husar@gmail.com>
2014-07-11 15:12:48 +0100

    add github hook

---
+++ hook.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.get('/generate', function(req, res){
+  console.dir(req.body);
+  res.json({done: true});
+});
+
+app.listen(3001);
