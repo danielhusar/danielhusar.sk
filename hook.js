33efa1a69f71060116f8552a969e3b756fed3aef
Daniel Husar <dano.husar@gmail.com>
2014-07-11 15:17:24 +0100

    run gulp

--- hook.js
+++ hook.js
@@ -5,7 +5,10 @@ var app = express();
 
 app.post('/generate', function(req, res){
   console.dir(req.body);
-  res.json({done: true});
+  require('child_process').exec('gulp', function(err, stdout, stderr) {
+    res.json({done: true});
+  });
+
 });
 
 app.listen(3001);
