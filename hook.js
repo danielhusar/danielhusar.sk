7a4533111d9583278f4afeac9cbe7226cb11f440
Daniel <dano.husar@gmail.com>
2014-08-09 22:51:33 +0100

    post

--- hook.js
+++ hook.js
@@ -3,25 +3,25 @@
 var express = require('express');
 var xhub = require('express-x-hub');
 var exec = require('exec-chainable');
-//var secret = require('./hook.json').secret;
+var secret = require('./hook.json').secret;
 
 var app = express();
-//app.use(xhub({ algorithm: 'sha1', secret: secret }));
+app.use(xhub({ algorithm: 'sha1', secret: secret }));
 
 app.get('/', function(req, res){
   res.json({running: true});
 });
 
-app.get('/generate', function(req, res) {
-
-    exec('git pull').then(function (a) {
-      console.log(a);
+app.post('/generate', function(req, res) {
+  if (req.isXHub) {
+    exec('git pull').then(function () {
       return exec('gulp');
-    }).done( function (a) {
-       console.log(a);
+    }).done( function () {
       res.json({error: false});
     });
-
+  } else {
+    res.json({error: true}, 404);
+  }
 });
 
 app.listen(3001);
