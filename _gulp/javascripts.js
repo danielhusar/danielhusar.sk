fc39ca613ab6e636f65c73f85e3056d83a43272a
Daniel Husar <dano.husar@gmail.com>
2014-08-23 18:21:37 +0100

    Update javascripts.js

--- _gulp/javascripts.js
+++ _gulp/javascripts.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var jshint = require('gulp-jshint');
-var stylish = require('jshint-stylish');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 
 var insert = function (obj, index, item) {
   obj.splice(index, 0, item);
 };
 
-module.exports = function(gulp, jsFiles){
+module.exports = function (gulp, jsFiles) {
 
   //lint js files
   gulp.task('lint', function () {
