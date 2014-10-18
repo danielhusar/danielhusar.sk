4f22277015d286e0c595786baa549a1276467af5
Daniel Husar <dano.husar@gmail.com>
2014-10-18 19:15:54 +0100

    Update events.js

--- _js/events.js
+++ _js/events.js
@@ -1,16 +1,16 @@
 /** events **/
-(function(window, document){
+(function (window, document) {
   'use strict';
 
   //wait a little and add class to body so we can trigger some animations
-  window.setTimeout(function(){
+  window.setTimeout(function () {
     document.getElementsByTagName('body')[0].className += ' page-loaded';
   }, 100);
 
   //open discussion
   var commentsTrigger = document.querySelector('[data-trigger="comments"]');
-  if(commentsTrigger){
-    commentsTrigger.addEventListener('click', function(e){
+  if (commentsTrigger) {
+    commentsTrigger.addEventListener('click', function (e) {
       e.preventDefault();
       this.className += ' hidden';
       var dsq = document.createElement('script');
@@ -22,9 +22,9 @@
 
   //history back
   var historyTrigger = document.querySelector('[data-trigger="back"]');
-  if(historyTrigger){
-    historyTrigger.addEventListener('click', function(e){
-      if(document.referrer && document.referrer.indexOf(window.location.origin) !== -1 && window.history.back){
+  if (historyTrigger) {
+    historyTrigger.addEventListener('click', function (e) {
+      if (document.referrer && document.referrer.indexOf(window.location.origin) !== -1 && window.history.back) {
         e.preventDefault();
         window.history.back();
       }
@@ -34,7 +34,7 @@
   //open popup
   var popupTriggers = document.querySelectorAll('[data-trigger="popup"]');
   var popupTrigger;
-  var popupOpener = function(e){
+  var popupOpener = function (e) {
     var left = (window.screen.width / 2)  - (700 / 2);
     var top = (window.screen.height / 2) - (500 / 2);
     var href = this.href;
@@ -44,8 +44,8 @@
                 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=700, height=500, top=' + top + ', left=' + left);
     e.preventDefault();
   };
-  for(var i = 0, length = popupTriggers.length; i < length; i++){
-    if(popupTriggers[i]){
+  for (var i = 0, length = popupTriggers.length; i < length; i++) {
+    if (popupTriggers[i]) {
        popupTriggers[i].addEventListener('click', popupOpener, false);
     }
   }
