46876210fd9858b6ef1d35d30457e46eea90eef7
Daniel Husar <dano.husar@gmail.com>
2015-05-20 17:05:12 +0100

    Add first test

--- test/test.js
+++ test/test.js
@@ -3,7 +3,9 @@
 var assert = require('assert');
 var build = require('../metalsmith.js');
 
-
-it('It should work', function () {
-  assert(true);
+it('It should not throw error when generating site', function (cb) {
+  build(function (err) {
+    assert.equal(err, null);
+    cb();
+  });
 });
