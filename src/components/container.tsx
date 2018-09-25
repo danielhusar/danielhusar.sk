307f51c9b775d1fd7e595f7833c1448b8b55dbbd
Daniel Husar <dano.husar@gmail.com>
2018-09-24 22:37:36 -0700

    First version

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -3,9 +3,12 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   margin: 0 auto;
-  max-width: 960px;
-  padding: 0px 1.0875rem 1.45rem;
-  padding-top: 0;
+  padding: 0 40px;
+  max-width: 1000px;
+
+  @media (max-width: 600px) {
+    padding: 0 20px;
+  }
 `;
 
 interface Props {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,40 +2,109 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
 import styled from 'styled-components';
+import { Flex, Box } from 'reflexbox';
+import { SocialIcon } from 'react-social-icons';
 import Layout from '../components/layout';
 import { File } from '../types/graphql';
 
 interface Props {
   data: {
-    file: File;
+    portrait: File;
+    intercom: File;
   };
 }
 
 const Headline = styled.h1`
   font-size: 40px;
-  margin: 50px 0 80px;
+  padding-top: 50px;
+  margin: 0 0 30px;
 
   @media (max-width: 600px) {
     font-size: 30px;
+    padding-top: 30px;
   }
 `;
+
+const SubHeadline = styled.h2`
+  font-size: 22px;
+  margin: -5px 0 20px;
+`;
+
+const Portrait = styled.div`
+  border-radius: 5px;
+  overflow: hidden;
+`;
+
+const IntercomLogo = styled.div`
+  display: inline-block;
+  position: relative;
+  top: 1px;
+  padding-right: 5px;
+`;
+
+const About = styled.div`
+  font-size: 20px;
+  margin-bottom: 20px;
+  max-width: 500px;
+`;
+
+const iconStyle = {
+  height: 40,
+  width: 40,
+  marginRight: '10px',
+};
 
 export default ({ data }: Props) => (
   <Layout>
     <Headline>Daniel Husar</Headline>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    {data.file.childImageSharp && data.file.childImageSharp.fixed ? (
-      <Img fixed={data.file.childImageSharp.fixed} alt="My portrait" fadeIn={false} />
-    ) : null}
+    <Flex wrap={true}>
+      <Box w={[1, 1 / 3]}>
+        {data.portrait.childImageSharp && data.portrait.childImageSharp.fluid ? (
+          <Portrait>
+            <Img fluid={data.portrait.childImageSharp.fluid} alt="My portrait" fadeIn={false} />
+          </Portrait>
+        ) : null}
+      </Box>
+      <Box w={[1, 2 / 3]} pl={[0, 4]} pt={[3, 0]}>
+        <About>
+          <SubHeadline>Hi!</SubHeadline>
+          My name is Daniel and I’m product engineer based in San Francisco.
+          <br />
+          <br />
+          Right now I’m working on{' '}
+          {data.intercom.childImageSharp && data.intercom.childImageSharp.fixed ? (
+            <IntercomLogo>
+              <Img fixed={data.intercom.childImageSharp.fixed} alt="Intercom" fadeIn={false} />
+            </IntercomLogo>
+          ) : null}
+          <a href="https://www.intercom.com">Intercom</a> <a href="https://www.intercom.com/messenger">Messenger</a> to make internet business
+          personal.
+          <br />
+          <br />
+          You learn more about me here:
+        </About>
+        <SocialIcon url="http://twitter.com/DanoHusar" style={iconStyle} />
+        <SocialIcon url="https://github.com/danielhusar" style={iconStyle} />
+        <SocialIcon url="https://www.linkedin.com/in/daniel-husar-60783958/" style={iconStyle} />
+        <SocialIcon url="https://www.instagram.com/efrafa/" style={iconStyle} />
+        <SocialIcon url="mailto:dano.husar@gmail.com" style={iconStyle} />
+      </Box>
+    </Flex>
   </Layout>
 );
 
 export const query = graphql`
   query {
-    file(relativePath: { eq: "portrait.jpeg" }) {
+    portrait: file(relativePath: { eq: "portrait.jpeg" }) {
       childImageSharp {
-        fixed(width: 400) {
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    intercom: file(relativePath: { eq: "intercom.png" }) {
+      childImageSharp {
+        fixed(width: 16) {
           ...GatsbyImageSharpFixed
         }
       }
