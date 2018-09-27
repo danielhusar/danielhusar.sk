13825ac0535b6cec5832f7fd68e9d318311b79cf
Daniel Husar <dano.husar@gmail.com>
2018-09-26 19:55:01 -0700

    Add travis config for deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,18 +8,6 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: 'Daniel Husar',
-        short_name: 'Daniel Husar',
-        start_url: '/',
-        background_color: '#209cee',
-        theme_color: '#209cee',
-        display: 'minimal-ui',
-        icon: 'src/images/icon.png', // This path is relative to the root of the site.
-      },
-    },
-    {
       resolve: 'gatsby-plugin-favicon',
       options: {
         logo: './src/images/icon.png',
