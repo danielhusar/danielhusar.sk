5cddd56854f3995201eb65638a44d03c2da8a4b5
Daniel Husar <dano.husar@gmail.com>
2018-10-14 21:40:02 -0700

    Fix spacings units

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
 export default styled.div`
-  margin: -1em 0;
+  margin: -0.7em 0;
 
   p {
     display: flex;
     flex-wrap: wrap;
-    margin: 0 -1em !important;
+    margin: 0 -0.7em !important;
   }
 
   p > .gatsby-resp-image-link,
@@ -14,8 +14,8 @@ export default styled.div`
     flex-basis: calc(33.333%);
     width: calc(33.333%);
     max-width: calc(33.333%) !important;
-    margin: 1em 0;
-    padding: 0 1em;
+    margin: 0.7em 0;
+    padding: 0 0.7em;
   }
 
   @media (max-width: 700px) {
