ef9dfed1227ea37e4aa9b9c544d5fbe68e9dbeec
Daniel Husar <dano.husar@gmail.com>
2020-05-27 18:12:14 -0700

    Fix blog posts

--- src/@daniel.husar/gatsby-theme-spring/components/header.tsx
+++ src/@daniel.husar/gatsby-theme-spring/components/header.tsx
@@ -9,7 +9,7 @@ const query = graphql`
   query {
     portrait: file(relativePath: { eq: "img/author.png" }) {
       childImageSharp {
-        fixed(width: 150, height: 150) {
+        fixed(width: 200, height: 200) {
           ...GatsbyImageSharpFixed
         }
       }
@@ -47,7 +47,7 @@ export default function Header() {
       {image ? <Img fixed={image} alt="" /> : null}
       <H1>Hi there!</H1>
       <p>My name is Daniel, and I’m a product engineer based in San Francisco.</p>
-      <p>Right now I’m working on Intercom Messenger to make internet business personal. Check out my blog or follow me on social media.</p>
+      <p>Right now I’m working on Intercom Messenger to make internet business personal.</p>
       <div>
         <SocialIcon url="https://twitter.com/danohusar" style={{ height: 35, width: 35 }} />
         <SocialIcon url="https://github.com/danielhusar" style={{ height: 35, width: 35 }} />
