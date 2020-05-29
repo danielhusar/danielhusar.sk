4453882e7fdfa4f37c0315e45abcc9f3f4d9468f
Daniel Husar <dano.husar@gmail.com>
2020-05-29 13:07:58 -0700

    Bring back images

--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import styled from '@emotion/styled'
 import { FiPhone, FiCompass, FiGithub, FiMail, FiTwitter } from 'react-icons/fi'
 
@@ -8,24 +9,11 @@ const Wrap = styled.div`
   max-width: 840px;
   margin: 0 auto;
   display: grid;
-  grid-template-columns: 280px 1fr;
+  grid-template-columns: 260px 1fr;
   font-size: 16px;
   font-weight: 300;
-  height: 100%;
   overflow: hidden;
   position: relative;
-
-  /* &:before {
-    content: '';
-    width: 120%;
-    height: 200px;
-    position: absolute;
-    top: -112px;
-    left: 0;
-    right: 0;
-    transform: rotate(10deg);
-    background: #323244;
-  } */
 
   a {
     color: inherit;
@@ -41,14 +29,19 @@ const Sidebar = styled.div`
   background: ${SIDEBAR_COLOR};
   color: white;
   padding: 40px;
-  min-height: 100%;
-  height: 100vh;
+  min-height: 100vh;
 `
 
 const Content = styled.div`
   background: white;
   padding: 40px;
   min-height: 100%;
+
+  ul {
+    margin: 10px 0 0;
+    padding-left: 18px;
+    font-size: 15px;
+  }
 
   li + li {
     margin-top: 5px;
@@ -57,25 +50,23 @@ const Content = styled.div`
 
 const Profile = styled.img`
   border-radius: 5px;
-  margin-top: 40px;
-  margin-top: 0;
 `
 
 const H1 = styled.h1`
   font-size: 40px;
   text-transform: uppercase;
-  margin: 161px 0 20px;
-  margin-top: 0;
+  margin: 0 0 20px;
   line-height: 1;
   letter-spacing: 5px;
   color: ${SIDEBAR_COLOR};
 `
 
 const H2 = styled.h2`
-  font-size: 25px;
+  font-size: 22px;
   text-transform: uppercase;
   margin: 40px 0 20px;
   letter-spacing: 1px;
+  font-weight: 500;
 `
 const ContentH2 = styled(H2)`
   color: ${SIDEBAR_COLOR};
@@ -107,55 +98,47 @@ const Spacer = styled.div<{ height: number | string }>`
   height: ${({ height }) => height}px;
 `
 
+const Small = styled.div`
+  font-size: 14px;
+`
+
+const Uppercase = styled.div`
+  text-transform: uppercase;
+`
+
 const Year = styled.h4`
-  margin: 40px 0 0;
+  margin: -4px 0 0;
+  font-size: 15px;
   font-weight: 300;
   text-transform: uppercase;
 `
 
-const CompanyHeader = styled.div`
-  margin-top: 10px;
+const Position = styled.h4`
+  font-size: 18px;
+  margin: 5px 0;
+  font-weight: 400;
+  line-height: 1;
+`
+
+const PositionHeader = styled.div`
+  margin: 40px 0 0;
   display: grid;
-  grid-template-columns: 70px 1fr;
+  grid-template-columns: 50px 1fr;
 
   img {
     border-radius: 5px;
   }
-
-  h4 {
-    font-size: 20px;
-    margin: -2px 0 5px;
-    font-weight: 400;
-    line-height: 1;
-  }
-
-  h5 {
-    font-size: 15px;
-    margin: 0 0 5px;
-    font-weight: 400;
-    line-height: 1;
-  }
-
-  h6 {
-    font-size: 15px;
-    margin: 0;
-    color: #828282;
-    font-weight: 400;
-  }
-`
-
-const PageBreak = styled.div`
-  page-break-after: always;
 `
 
 export default function CV() {
   return (
     <>
+      <Helmet>
+        <title>Daniel Husar</title>
+      </Helmet>
       <Wrap>
         <Sidebar>
-          <Profile src="/profile.jpeg" width="200" />
-
-          <H2>Get in touch!</H2>
+          <H2 style={{ marginTop: 0 }}>Get in touch!</H2>
           <HR width="100" />
           <Contact>
             <FiMail size="20" /> <a href="mailto:dano.husar@gmail.com">dano.husar@gmail.com</a>
@@ -174,6 +157,18 @@ export default function CV() {
           <HR width="100" />
           <Spacer height={20} />
           <div>English, Slovak, Czech, German (basics)</div>
+
+          <H2>Education</H2>
+          <HR width="100" />
+          <Spacer height={20} />
+          <div>
+            <Small>
+              <Uppercase>Sep 2004 - Jun 2009</Uppercase>
+            </Small>
+            <a href="https://www.tuke.sk/">Technical University of Kosice</a>
+          </div>
+
+          <div>Engineer's degree</div>
         </Sidebar>
         <Content>
           <H1>Daniel Husar</H1>
@@ -182,17 +177,15 @@ export default function CV() {
           <ContentH2>Experience</ContentH2>
           <HR width="100" />
 
-          <Year>Aug 2016 - Present</Year>
-          <CompanyHeader>
-            <img src="/intercom.svg" width="60" />
+          <PositionHeader>
+            <img src="/intercom.svg" width="40" />
             <div>
-              <h4>
-                <a href="https://www.intercom.com/">Intercom</a>
-              </h4>
-              <h5>Product Engineer</h5>
-              <h6>San Francisco, California</h6>
+              <Year>
+                Aug 2016 - Present &nbsp;•&nbsp; <a href="https://www.intercom.com/">Intercom</a>
+              </Year>
+              <Position>Product Engineer - San Francisco, California</Position>
             </div>
-          </CompanyHeader>
+          </PositionHeader>
           <ul>
             <li>
               Tech lead on the messenger accessibility project (<a href="https://www.intercom.com/blog/messenger-accessibility/">blog post</a>)
@@ -204,87 +197,62 @@ export default function CV() {
             <li>Tech lead on sass to css-in-js (emotion) rewrite in the messenger</li>
           </ul>
 
-          <Year>May 2019 - Present</Year>
-          <CompanyHeader>
-            <img src="/slovensko-digital.png" width="60" />
+          <PositionHeader>
+            <img src="/slovensko-digital.png" width="40" />
             <div>
-              <h4>
-                <a href="https://slovensko.digital/">Slovensko Digital (non profit organization)</a>
-              </h4>
-              <h5>Full Stack Engineer</h5>
-              <h6>San Francisco, California</h6>
+              <Year>
+                May 2019 - Present &nbsp;•&nbsp; <a href="https://slovensko.digital/">Slovensko Digital</a>
+              </Year>
+              <Position>Full stack Engineer - San Francisco, California</Position>
             </div>
-          </CompanyHeader>
+          </PositionHeader>
+
           <ul>
-            <li>Tech lead on the online application project that helped people to vote for last parliament elections.</li>
+            <li>Tech lead on the online application project that helped people to vote in the last parliament elections.</li>
           </ul>
 
-          <Year>Sep 2014 - Aug 2016</Year>
-          <CompanyHeader>
-            <img src="/intercom.svg" width="60" />
+          <PositionHeader>
+            <img src="/intercom.svg" width="40" />
             <div>
-              <h4>
-                <a href="https://www.intercom.com/">Intercom</a>
-              </h4>
-              <h5>Product Engineer</h5>
-              <h6>Dublin, Ireland</h6>
+              <Year>
+                Sep 2014 - Aug 2016 &nbsp;•&nbsp; <a href="https://www.intercom.com/">Intercom</a>
+              </Year>
+              <Position>Product Engineer - Dublin, Ireland</Position>
             </div>
-          </CompanyHeader>
+          </PositionHeader>
           <ul>
             <li>Front-end lead on the Intercom help center articles product.</li>
             <li>Working closely with gtm, marketing and sales teams.</li>
           </ul>
 
-          <PageBreak />
-          <Year>Sep 2013 - Sep 2014</Year>
-          <CompanyHeader>
-            <img src="/each-and-other.png" width="60" />
+          <PositionHeader>
+            <img src="/each-and-other.png" width="40" />
             <div>
-              <h4>
-                <a href="https://www.eachandother.com/">Each and Other</a>
-              </h4>
-              <h5>Front-end Engineer</h5>
-              <h6>Dublin, Ireland</h6>
+              <Year>
+                Sep 2013 - Sep 2014 • <a href="https://www.eachandother.com/">Each and Other</a>
+              </Year>
+              <Position>Front-end Engineer - Dublin, Ireland</Position>
             </div>
-          </CompanyHeader>
+          </PositionHeader>
           <ul>
             <li>Front-end lead on the Pernament TSB new internet banking.</li>
             <li>Front-end lead on the Liberty Insurance Ireland new website.</li>
             <li>Helping shipping the new Vodafone Ireland site.</li>
           </ul>
 
-          <Year>Nov 2011 – Aug 2013</Year>
-          <CompanyHeader>
-            <img src="/perform.jpeg" width="60" />
+          <PositionHeader>
+            <img src="/perform.jpeg" width="40" />
             <div>
-              <h4>
-                <a href="https://www.eachandother.com/">Perform Group</a>
-              </h4>
-              <h5>Web Developer</h5>
-              <h6>Kosice, Slovakia</h6>
+              <Year>
+                Nov 2011 – Aug 2013 &nbsp;•&nbsp; <a href="https://www.eachandother.com/">Perform Group</a>
+              </Year>
+              <Position>Web Developer - Kosice Slovakia</Position>
             </div>
-          </CompanyHeader>
+          </PositionHeader>
+
           <ul>
-            <li>Front-end lead on the Pernament TSB new internet banking.</li>
-            <li>Front-end lead on the Liberty Insurance Ireland new website.</li>
-            <li>Helping shipping the new Vodafone Ireland site.</li>
+            <li>Working on biggest live sport streaming service in UK.</li>
           </ul>
-
-          <Spacer height={20} />
-          <ContentH2>Education</ContentH2>
-          <HR width="100" />
-
-          <Year>Sep 2004 - Jun 2009</Year>
-          <CompanyHeader>
-            <img src="/tuke.png" width="60" />
-            <div>
-              <h4>
-                <a href="https://www.tuke.sk/">Technical University of Kosice</a>
-              </h4>
-              <h5>Engineer's degree, Artificial Intelligence</h5>
-              <h6>Kosice, Slovakia</h6>
-            </div>
-          </CompanyHeader>
         </Content>
       </Wrap>
       <style>
@@ -293,11 +261,13 @@ export default function CV() {
           body,
           #___gatsby,
           #gatsby-focus-wrapper {
-            min-height: 100%;
+            min-height: 100vh;
             padding: 0;
             margin: 0;
             -webkit-font-smoothing: antialiased;
           }
+
+
         `}
       </style>
     </>
